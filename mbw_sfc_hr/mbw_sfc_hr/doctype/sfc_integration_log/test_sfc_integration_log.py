10afad1cc246bd7686af76cd58d02595d939111f
Tuan Bui <antivenomff@gmail.com>
2024-02-28 09:47:42 +0700

    delete doctype

--- mbw_sfc_hr/mbw_sfc_hr/doctype/sfc_integration_log/test_sfc_integration_log.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, mbw and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestSfcIntegrationLog(FrappeTestCase):
-	pass
--- mbw_sfc_hr/mbw_sfc_hr/doctype/sfc_setting/sfc_setting.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, mbw and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("SFC Setting", {
-// 	refresh(frm) {
-
-// 	},
-// });

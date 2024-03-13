ab9d677a699b9fb9c0acb072646d7b24ef4b69d0
Tuan Bui <antivenomff@gmail.com>
2024-03-13 18:55:54 +0700

    attendance monthly

---
+++ mbw_sfc_hr/sfc_hr/doctype/sfc_attendance_monthly_report/sfc_attendance_monthly_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, MBW and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("SFC Attendance Monthly Report", {
+// 	refresh(frm) {
+
+// 	},
+// });

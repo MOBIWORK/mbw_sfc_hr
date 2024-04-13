d4e2a05abbad28f8d988c9104f9f83dfed6adc95
chuyendev <chuyendev@gmail.com>
2024-04-13 11:17:17 +0700

    update date column att

--- mbw_sfc_desk/src/sections/Worksheet/component/renderColumn.tsx
+++ mbw_sfc_desk/src/sections/Worksheet/component/renderColumn.tsx
@@ -4,32 +4,280 @@ export interface column {
     parent_key: string | null,
     key: string,
     title: string,
-    children: column[] | []
+    children: column[] | [],
+    type_column?: string
 }
 interface renderProps {
     data_column: column,
     fix_left_column: string[],
     fix_right_column: string[],
+    cb?:any
 }
 
 const { Column, ColumnGroup } = TableCustom;
-export function renderColumn({ data_column, fix_left_column = [], fix_right_column = [] }: renderProps) {
+export function renderColumn({ data_column, fix_left_column = [], fix_right_column = [],cb }: renderProps) {
     if (data_column.children?.length > 0) {
-        return <ColumnGroup title={data_column.title} className="!min-w-[670px]" {...fix_left_column.includes(data_column.key) && { fixed: "left" }}
+        return <ColumnGroup title={data_column.title} {...fix_left_column.includes(data_column.key) && { fixed: "left" }}
             {...fix_right_column.includes(data_column.key) && { fixed: "right" }}>
             {
                 data_column.children?.map(dt => renderColumn({ data_column: dt, fix_left_column, fix_right_column }))
             }
         </ColumnGroup>
     }
-
-    return <Column
+    if(data_column.type_column == "date") {
+        return <Column
         {...fix_left_column.includes(data_column.key) && { fixed: "left" }}
         {...fix_right_column.includes(data_column.key) && { fixed: "right" }}
         className="!text-center"
         title={data_column.title}
         dataIndex={data_column.key}
         key={data_column.key}
+        render={(value,record) => renderDateColumn(value, record,cb)
+
+        }
     />
+    }
+    else {
+        return <Column
+            {...fix_left_column.includes(data_column.key) && { fixed: "left" }}
+            {...fix_right_column.includes(data_column.key) && { fixed: "right" }}
+            className="!text-center"
+            title={data_column.title}
+            dataIndex={data_column.key}
+            key={data_column.key}
+        />
+
+    }
 
 }
+
+function renderDateColumn(value: any, record: any,cb:any) {
+    if (
+      value?.dayOfWeek === "Thứ 7" ||
+      value?.dayOfWeek === "Chủ nhật"
+    ) {
+      return (
+        <div className="bg-gray-300 !h-14 !text-center flex justify-center items-center -m-4">
+          OFF
+        </div>
+      );
+    }
+    if (value.work_hours !== 0 && !value.work_hours) {
+      switch (value?.sign) {
+        case "HE":
+          return (
+            <div
+              onClick={() => {
+                // console.log(record.employee, value.att_day);
+                cb({
+                  open: true,
+                  id: {
+                    employee: record.employee,
+                    att_day: value.att_day,
+                  },
+                });
+              }}
+              className="text-red-700 !h-14 flex justify-center items-center"
+            >
+              {value?.work_hours}
+            </div>
+          );
+          break;
+        case "FID":
+          return (
+            <div
+              onClick={() => {
+                // console.log(record.employee, value.att_day);
+                cb({
+                  open: true,
+                  id: {
+                    employee: record.employee,
+                    att_day: value.att_day,
+                  },
+                });
+              }}
+              className="border-solid border-[red] !h-14 flex justify-center items-center"
+            >
+              {value?.work_hours}
+            </div>
+          );
+          break;
+        case "ON":
+          return (
+            <div
+              onClick={() => {
+                // console.log(record.employee, value.att_day);
+                cb({
+                  open: true,
+                  id: {
+                    employee: record.employee,
+                    att_day: value.att_day,
+                  },
+                });
+              }}
+              className="text-yellow-500 !h-14 flex justify-center items-center"
+            >
+              {value?.work_hours}
+            </div>
+          );
+          break;
+        case "EA":
+          return (
+            <div
+              onClick={() => {
+                // console.log(record.employee, value.att_day);
+                cb({
+                  open: true,
+                  id: {
+                    employee: record.employee,
+                    att_day: value.att_day,
+                  },
+                });
+              }}
+              className="text-green-500 !h-14 flex justify-center items-center"
+            >
+              v
+            </div>
+          );
+          break;
+        default:
+          return <div>x</div>;
+      }
+    }
+
+    switch (value?.sign) {
+      case "HE":
+        return (
+          <div
+            onClick={() => {
+              // console.log(record.employee, value.att_day);
+              cb({
+                open: true,
+                id: {
+                  employee: record.employee,
+                  att_day: value.att_day,
+                  employee_name: record.employee_name,
+                  day: value.dayOfWeek,
+                },
+              });
+            }}
+            className="text-red-700 !h-14 flex justify-center items-center"
+          >
+            {value?.work_hours}
+          </div>
+        );
+        break;
+      case "FID":
+        return (
+          <div
+            onClick={() => {
+              // console.log(record.employee, value.att_day);
+              cb({
+                open: true,
+                id: {
+                  employee: record.employee,
+                  att_day: value.att_day,
+                  employee_name: record.employee_name,
+                  day: value.dayOfWeek,
+                },
+              });
+            }}
+            className="border-solid border-[red] !h-14 flex justify-center items-center"
+          >
+            {value?.work_hours}
+          </div>
+        );
+        break;
+      case "ON":
+        return (
+          <div
+            onClick={() => {
+              // console.log(record.employee, value.att_day);
+              cb({
+                open: true,
+                id: {
+                  employee: record.employee,
+                  att_day: value.att_day,
+                  employee_name: record.employee_name,
+                  day: value.dayOfWeek,
+                },
+              });
+            }}
+            className="text-yellow-500 !h-14 flex justify-center items-center"
+          >
+            {value?.work_hours}
+          </div>
+        );
+        break;
+      case "EA":
+        return (
+          <div
+            onClick={() => {
+              // console.log(record.employee, value.att_day);
+              cb({
+                open: true,
+                id: {
+                  employee: record.employee,
+                  att_day: value.att_day,
+                  employee_name: record.employee_name,
+                  day: value.dayOfWeek,
+                },
+              });
+            }}
+            className="text-green-500 !h-14 flex justify-center items-center"
+          >
+            v
+          </div>
+        );
+        break;
+      case "+":
+      case "P":
+      case "KL":
+      case "VM":
+      case "OT":
+      case "CT":
+      case "CD":
+      case "DC":
+      case "GT":
+        return (
+          <div
+            onClick={() => {
+              // console.log(record.employee, value.att_day);
+              cb({
+                open: true,
+                id: {
+                  employee: record.employee,
+                  att_day: value.att_day,
+                  employee_name: record.employee_name,
+                  day: value.dayOfWeek,
+                },
+              });
+            }}
+          >
+            {value?.work_hours}
+            <sup>{value?.sign}</sup>
+          </div>
+        );
+        break;
+      default:
+        return (
+          <div
+            onClick={() => {
+              // console.log(record.employee, value.att_day);
+              cb({
+                open: true,
+                id: {
+                  employee: record.employee,
+                  att_day: value.att_day,
+                  employee_name: record.employee_name,
+                  day: value.dayOfWeek,
+                },
+              });
+            }}
+            className="!h-14 flex justify-center items-center"
+          >
+            {value?.work_hours || " "}{" "}
+          </div>
+        );
+    }
+  }

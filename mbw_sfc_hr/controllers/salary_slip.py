6878af39db9a18f7a24fd96a7297432dc69b205e
Tuan Bui <antivenomff@gmail.com>
2024-07-10 09:25:30 +0700

    fix salary slip

--- mbw_sfc_hr/controllers/salary_slip.py
+++ mbw_sfc_hr/controllers/salary_slip.py
@@ -7,24 +7,25 @@ def salary_slip_update(doc, method):
     month = start_date.month
     year = start_date.year
 
-    salary_structure = frappe.get_doc("Salary Structure", doc.salary_structure).as_dict()
-    earnings = salary_structure.earnings
-    deductions = salary_structure.deductions
+    if doc.salary_structure:
+        salary_structure = frappe.get_doc("Salary Structure", doc.salary_structure).as_dict()
+        earnings = salary_structure.earnings
+        deductions = salary_structure.deductions
 
-    if earnings:
-        for i in earnings:
-            salary_components = frappe.get_value("Salary Component", {"name": i.salary_component, "doctype_data": 1, "type": "Earning"}, ["custom_doctype", "field_name"], as_dict=True)
-            if salary_components:
-                value = frappe.get_value(salary_components.custom_doctype , {'employee': employee, 'month': month, 'year': year}, salary_components.field_name)
-                for k in doc.earnings:
-                    if k.salary_component == i.salary_component:
-                        k.amount = value
-    
-    if deductions:
-        for a in deductions:
-            salary_components = frappe.get_value("Salary Component", {"name": a.salary_component, "doctype_data": 1, "type": "Deduction"}, ["custom_doctype", "field_name"], as_dict=True)
-            if salary_components:
-                value = frappe.get_value(salary_components.custom_doctype , {'employee': employee, 'month': month, 'year': year}, salary_components.field_name)
-                for j in doc.deductions:
-                    if j.salary_component == a.salary_component:
-                        j.amount = value
+        if earnings:
+            for i in earnings:
+                salary_components = frappe.get_value("Salary Component", {"name": i.salary_component, "doctype_data": 1, "type": "Earning"}, ["custom_doctype", "field_name"], as_dict=True)
+                if salary_components:
+                    value = frappe.get_value(salary_components.custom_doctype , {'employee': employee, 'month': month, 'year': year}, salary_components.field_name)
+                    for k in doc.earnings:
+                        if k.salary_component == i.salary_component:
+                            k.amount = value
+        
+        if deductions:
+            for a in deductions:
+                salary_components = frappe.get_value("Salary Component", {"name": a.salary_component, "doctype_data": 1, "type": "Deduction"}, ["custom_doctype", "field_name"], as_dict=True)
+                if salary_components:
+                    value = frappe.get_value(salary_components.custom_doctype , {'employee': employee, 'month': month, 'year': year}, salary_components.field_name)
+                    for j in doc.deductions:
+                        if j.salary_component == a.salary_component:
+                            j.amount = value

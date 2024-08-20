769972f5ab93a206897b1ab1cef821d4c7099477
Tuan Bui <antivenomff@gmail.com>
2024-08-20 16:15:57 +0700

    fix salary slip

--- mbw_sfc_hr/controllers/salary_slip.py
+++ mbw_sfc_hr/controllers/salary_slip.py
@@ -1,4 +1,5 @@
 import frappe
+import calendar
 from datetime import datetime
 
 def salary_slip_update(doc, method):
@@ -6,6 +7,13 @@ def salary_slip_update(doc, method):
     start_date = datetime.strptime(doc.start_date, "%Y-%m-%d")
     month = start_date.month
     year = start_date.year
+
+    # Lấy ngày đầu tiên của tháng
+    first_day = datetime(year, month, 1).date()
+
+    # Lấy ngày cuối cùng của tháng
+    last_day = calendar.monthrange(year, month)[1]
+    last_day_date = datetime(year, month, last_day).date()
 
     # Kiểm tra và tự động gán Salary Structure nếu chưa có
     if employee and not doc.salary_structure:
@@ -25,7 +33,11 @@ def salary_slip_update(doc, method):
             for i in earnings:
                 salary_component = frappe.get_value("Salary Component", {"name": i.salary_component, "doctype_data": 1, "type": "Earning"}, ["custom_doctype", "field_name"], as_dict=True)
                 if salary_component:
-                    value = frappe.get_value(salary_component.custom_doctype, {'nhan_vien_ban_hang': employee, 'thang': month, 'nam': year}, salary_component.field_name)
+                    value = None
+                    if salary_component.custom_doctype == "DMS KPI":
+                        value = frappe.get_value("DMS KPI", {"ngay_hieu_luc_tu": (">=", first_day), "ngay_hieu_luc_den": ("<=", last_day_date), "nhan_vien_ban_hang": employee}, salary_component.field_name)
+                    else:
+                        value = frappe.get_value(salary_component.custom_doctype, {"nhan_vien_ban_hang": employee, "thang": month, "nam": year}, salary_component.field_name)
                     if value is not None:
                         for k in doc.earnings:
                             if k.salary_component == i.salary_component:
@@ -36,7 +48,11 @@ def salary_slip_update(doc, method):
             for a in deductions:
                 salary_component = frappe.get_value("Salary Component", {"name": a.salary_component, "doctype_data": 1, "type": "Deduction"}, ["custom_doctype", "field_name"], as_dict=True)
                 if salary_component:
-                    value = frappe.get_value(salary_component.custom_doctype, {'nhan_vien_ban_hang': employee, 'thang': month, 'nam': year}, salary_component.field_name)
+                    value = None
+                    if salary_component.custom_doctype == "DMS KPI":
+                        value = frappe.get_value("DMS KPI", {"ngay_hieu_luc_tu": (">=", first_day), "ngay_hieu_luc_den": ("<=", last_day_date), "nhan_vien_ban_hang": employee}, salary_component.field_name)
+                    else:
+                        value = frappe.get_value(salary_component.custom_doctype, {"nhan_vien_ban_hang": employee, "thang": month, "nam": year}, salary_component.field_name)
                     if value is not None:
                         for j in doc.deductions:
                             if j.salary_component == a.salary_component:
